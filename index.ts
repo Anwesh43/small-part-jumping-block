3f8a622b791ea1ebc39d9fedca096a008d2e6744
Anwesh43 <anweshthecool0@gmail.com>
2020-07-12 04:21:00 +0530

    adding SJBNode class

--- index.ts
+++ index.ts
@@ -130,3 +130,49 @@ class Animator {
         }
     }
 }
+
+class SJBNode {
+
+    prev : SJBNode
+    next : SJBNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new SJBNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawJBNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SJBNode {
+        var curr : SJBNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+}
