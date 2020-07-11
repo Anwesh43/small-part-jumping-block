6836dabeb49e05b985960b40b7b9a6cc07e51917
Anwesh43 <anweshthecool0@gmail.com>
2020-07-12 03:56:45 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -8,6 +8,21 @@ const backColor : string = "#BDBDBD"
 const delay : number = 20
 const nodes : number = 5
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static sinify(scale : number) {
+        return Math.sin(scale * Math.PI)
+    }
+}
+
 class Stage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D = this.canvas.getContext('2d')
